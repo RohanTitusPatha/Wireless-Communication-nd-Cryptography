5d78cab695f55746025f044314ffb2dd1e67c70b
Rohan Titus <rohantituspatha@gmail.com>
2023-12-03 03:54:11 +0530

    non-ecc codes

---
+++ Transmitter_nonecc_code.py
@@ -0,0 +1,33 @@
+#include <RH_ASK.h>
+
+RH_ASK radio(2000, 11, 12);
+const int maxLength = 128; // Maximum length of the input
+char inputBuffer[maxLength]; // Buffer to store the input
+
+void setup() {
+    Serial.begin(9600);
+
+    if (!radio.init()) {
+        Serial.println("Radio module failed to initialize");
+    }
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+        // Read characters from the Serial Monitor and store them in the buffer
+        int bytesRead = Serial.readBytesUntil('\n', inputBuffer, maxLength - 1);
+
+        // Null-terminate the string (add a null character at the end)
+        inputBuffer[bytesRead] = '\0';
+
+        // Send the message
+        radio.send((uint8_t*)inputBuffer, bytesRead);
+        radio.waitPacketSent();
+
+        // Print information to the Serial Monitor
+        Serial.println("Sent");
+        Serial.println("Message: " + String(inputBuffer));
+    }
+
+    delay(1000);
+}
